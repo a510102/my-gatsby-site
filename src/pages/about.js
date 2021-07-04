a75d3c0253ce86f8375f9228e0cd23af31c91fd0
jay <b98310047@hotmail.com>
2021-07-04 23:43:05 +0800

    finished gatsby part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const AboutMe = () => {
+  return (
+    <Layout pageTitle='About Me'>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  )
+};
+
+export default AboutMe;
